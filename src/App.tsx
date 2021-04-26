eb857f080d339e58764a72e07aae8bd3e229d082
Vlad Denisov <vlad.a.denisov@gmail.com>
2021-04-26 13:00:19 +0300

    fix: mobile height

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import { useScreen } from './Contexts/ScreenContext';
 const App: React.FunctionComponent = ({children}) => {
   const {dispatch} = useScreen()
   useEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
     window.addEventListener('resize', (e) => {
       dispatch({type: 'CHANGE'})
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
     })
   }, [])
   return <>
